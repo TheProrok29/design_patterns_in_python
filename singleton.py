645592ecb1a919a97987c1ebb4c4e85e2de0a6d8
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-05 18:12:32 +0100

    Add example for singleton

--- singleton.py
+++ singleton.py
@@ -9,16 +9,10 @@ class Singleton:
         return cls._instance
 
 
-class MultiSingleton:
-    """Implementation for exact quantity instances"""
-    _instances = []
-    _instances_max = 3
-    _instances_actual = -1
-
-    def __new__(cls, *args, **kwargs):
-        if len(cls._instances) < cls._instances_max:
-            instance = super().__new__(cls, *args, *kwargs)
-            cls._instances.append(instance)
-        cls._instances_actual += 1
-        cls._instances_actual = cls._instances_actual % 3
-        return cls._instances[cls._instances_actual]
+# Example
+if __name__ == '__main__':
+    x = Singleton()
+    y = Singleton()
+    print(x is y)
+    print(id(x))
+    print(id(y))
