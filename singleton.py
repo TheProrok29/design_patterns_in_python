fb2778877d02596eb614c1616c531e8f99d9b20b
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-05 17:54:43 +0100

    Add Singleton implementation for exact quantity of instances.

--- singleton.py
+++ singleton.py
@@ -1,4 +1,5 @@
 class Singleton:
+    """Implementation for one instance"""
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -6,3 +7,18 @@ class Singleton:
             instance = super().__new__(cls, *args, *kwargs)
             cls._instance = instance
         return cls._instance
+
+
+class MultiSingleton:
+    """Implementation for exact quantity instances"""
+    _instances = []
+    _instances_max = 3
+    _instances_actual = -1
+
+    def __new__(cls, *args, **kwargs):
+        if len(cls._instances) < cls._instances_max:
+            instance = super().__new__(cls, *args, *kwargs)
+            cls._instances.append(instance)
+        cls._instances_actual += 1
+        cls._instances_actual = cls._instances_actual % 3
+        return cls._instances[cls._instances_actual]
