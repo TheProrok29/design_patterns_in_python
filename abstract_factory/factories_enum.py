44dd4b62ab7da2be43e88d2c76cc8636f746a9f9
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-15 19:11:04 +0100

    Add Enum class Factories to store factory type.

---
+++ abstract_factory/factories_enum.py
@@ -0,0 +1,8 @@
+from enum import Enum, auto
+
+
+class Factories(Enum):
+    ChildrenChairsFactory = auto()
+    ExclusiveChairsFactory = auto()
+    OfficeChairsFactory = auto()
+    PlaneChairsFactory = auto()
