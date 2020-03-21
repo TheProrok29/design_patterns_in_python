ed2406cf347cb18ce4c5b70737ced8a5ef7c9e95
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-21 18:37:21 +0100

    Add Prototype class.

---
+++ prototype/prototype.py
@@ -0,0 +1,20 @@
+from copy import deepcopy
+
+
+class Prototype:
+    def __init__(self):
+        self.objs = dict()
+
+    def add_prototype(self, id_, obj):
+        self.objs[id_] = obj
+
+    def del_prototype(self, id_):
+        del self.objs[id_]
+
+    def clone(self, id_, **kwargs):
+        if id_ in self.objs:
+            instance = deepcopy(self.objs[id_])
+            for key in kwargs:
+                setattr(instance, key, kwargs[key])
+        else:
+            raise ModuleNotFoundError
