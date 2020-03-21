4d3393e1931291b0fa1dd8a3e768b7f685c2647d
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-21 09:23:31 +0100

    Add DictBuilder class.

---
+++ builder/dict_builder.py
@@ -0,0 +1,17 @@
+import csv
+
+from builder import Builder
+
+
+class DictBuilder(Builder):
+    __data = None
+
+    def set_data(self, data):
+        self.__data = data
+
+    def save_data(self):
+        with open('dict_data.csv', 'w', newline='', encoding='UTF-8') as file:
+            keys = self.__data[0].keys()
+            dict_writer = csv.DictWriter(file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(self.__data)
