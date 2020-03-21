a645b68c20b9aac1cda74584ca3cf84897b06ada
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-21 19:38:14 +0100

    Add Dog class.

---
+++ structural_design_patterns/adapter/dog.py
@@ -0,0 +1,12 @@
+class Dog:
+    def __init__(self, age, sex, breed, marking):
+        self.age = age
+        self.sex = sex
+        self.breed = breed
+        self.marking = marking
+
+    def run(self):
+        print('I go')
+
+    def bark(self):
+        print('Hau Hau')
