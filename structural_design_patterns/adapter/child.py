0996992d1e32decd91832ccd97683adfea70cdfa
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-21 19:38:01 +0100

    Add Child class.

---
+++ structural_design_patterns/adapter/child.py
@@ -0,0 +1,11 @@
+class Child:
+    def __init__(self, age, sex, race):
+        self.age = age
+        self.sex = sex
+        self.race = race
+
+    def walk(self):
+        pass
+
+    def say_something(self, sth):
+        print(sth)
