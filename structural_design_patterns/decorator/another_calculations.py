e1e5125955b19e37268b8aa6d9d335192beef720
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-23 15:48:18 +0100

    Add some another calculations to test future decorator.

---
+++ structural_design_patterns/decorator/another_calculations.py
@@ -0,0 +1,9 @@
+def calculation(n):
+    summary = 0
+    if n % 2 == 0:
+        for i in range(1, n + 1, 2):
+            summary += i
+    else:
+        for i in range(2, n + 1, 2):
+            summary += i
+    return 5 * n ** 5 - 4 * n ** 4 - 3 * summary
