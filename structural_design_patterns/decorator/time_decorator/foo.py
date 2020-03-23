d67b71b6881d56ea647a208f303077ad1a5d64c2
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-23 16:25:28 +0100

    Add timer decorator to Foo methods to measure execute time.

--- structural_design_patterns/decorator/time_decorator/foo.py
+++ structural_design_patterns/decorator/time_decorator/foo.py
@@ -1,16 +1,22 @@
+from time_decorator import timer
+
+
 class Foo:
     def __init__(self, a: int, b: int, text: str):
         self.a = a
         self.b = b
         self.text = text
 
+    @timer
     def do_stuff1(self):
         return sum(x for x in range(self.a, self.b))
 
+    @timer
     def do_stuff2(self):
         for i in range(self.a * self.b):
             self.text = self.text[::-1]
 
+    @timer
     def do_stuff3(self):
         a, b = self.a ** 3, self.b ** 3
         while a != b:
@@ -18,4 +24,4 @@ class Foo:
                 if a > b:
                     a -= b
                 else:
-                    b -= a 
+                    b -= a
