1900bfe7655960875ad98bc380ff961628b19c28
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-23 16:19:38 +0100

    Add Foo operations class.

---
+++ structural_design_patterns/decorator/time_decorator/foo.py
@@ -0,0 +1,21 @@
+class Foo:
+    def __init__(self, a: int, b: int, text: str):
+        self.a = a
+        self.b = b
+        self.text = text
+
+    def do_stuff1(self):
+        return sum(x for x in range(self.a, self.b))
+
+    def do_stuff2(self):
+        for i in range(self.a * self.b):
+            self.text = self.text[::-1]
+
+    def do_stuff3(self):
+        a, b = self.a ** 3, self.b ** 3
+        while a != b:
+            if a != b:
+                if a > b:
+                    a -= b
+                else:
+                    b -= a 
