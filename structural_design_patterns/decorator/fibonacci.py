1d4514fa892667adc50b2b6d50b4bfe4310dc5e1
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-23 15:46:47 +0100

    Revert to simple reccursive fib function.

--- structural_design_patterns/decorator/fibonacci.py
+++ structural_design_patterns/decorator/fibonacci.py
@@ -1,25 +1,9 @@
-import time
-
-fib_called = 0
-
-cache = dict()
-
-
 def fib(n):
-    global fib_called
-    fib_called += 1
     if n < 2:
         return 1
-    if n in cache:
-        return cache[n]
-    result = fib(n - 1) + fib(n - 2)
-    cache[n] = result
-    return result
+    else:
+        return fib(n - 1) + fib(n - 2)
 
 
 if __name__ == '__main__':
-    start = time.time()
     print(fib(100))
-    stop = time.time()
-    print(fib_called)
-    print(stop - start)
