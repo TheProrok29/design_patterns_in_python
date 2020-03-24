27861fd82b9fdbff6094093fba8fdee633a57a50
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-24 15:07:09 +0100

    Add WarFetcher inherit from Fetcher.

---
+++ structural_design_patterns/bridge/war_fetcher.py
@@ -0,0 +1,15 @@
+from fetcher import Fetcher
+
+
+class WarFetcher(Fetcher):
+    def get_snippet(self):
+        return self.article[:80]
+
+    def get_article(self):
+        return self.article
+
+    def get_ads(self):
+        return 'Some weapon ads'
+
+    def go_to_full_version(self):
+        return 'Full version - now for only 5$ '
