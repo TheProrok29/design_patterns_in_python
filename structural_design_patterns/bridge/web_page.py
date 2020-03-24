c4e2503db2e07ba0c47ffbbf2e957e687ea24f51
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-24 15:48:07 +0100

    Add __str__() to WebPage to show interface and implementation.

--- structural_design_patterns/bridge/web_page.py
+++ structural_design_patterns/bridge/web_page.py
@@ -8,3 +8,6 @@ class WebPage:
     @abstractmethod
     def show_page(self):
         pass
+
+    def __str__(self):
+        return f'Interface: {self.__class__}, Implementation: {self.fetcher.__class__}'
