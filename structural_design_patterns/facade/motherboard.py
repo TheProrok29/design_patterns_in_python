9f18656b367c7d0d58dc40b0d0a87ff28a010b59
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-24 16:06:50 +0100

    Add Motherboard class.

---
+++ structural_design_patterns/facade/motherboard.py
@@ -0,0 +1,8 @@
+class Motherboard:
+    def __init__(self, cpu, memory, hard_drive, usbs, joints, size):
+        self.cpu = cpu
+        self.memory = memory
+        self.hard_drive = hard_drive
+        self.usbs = usbs
+        self.joints = joints
+        self.size = size
