df836df3e184db7581d55990426c5aa05a8f42ba
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-24 16:46:15 +0100

    Create CounterGame class.

---
+++ structural_design_patterns/flyweight/game.py
@@ -0,0 +1,5 @@
+class CounterGame:
+    types = ['Terrorist', 'Counter Terrorist']
+    weapons = ['AK-47', 'M4A1', 'SG552', 'AUG A1', 'Colt']
+    appearance = {'Terrorist': 'Ter1 Ter2 Ter3 Ter4'.split(),
+                  'Counter Terrorist': 'CTer1 CTer2 CTer3 CTer4'.split()}
