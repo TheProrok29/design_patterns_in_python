ba47b53b02d9fe66e10ace5e4ef3887c6559db53
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-24 17:00:31 +0100

    Move set weapon atribute to different method.

--- structural_design_patterns/flyweight/player.py
+++ structural_design_patterns/flyweight/player.py
@@ -2,12 +2,15 @@ from abc import ABC
 
 
 class Player(ABC):
-    def __init__(self, weapon, specific_appearance):
-        self.weapon = weapon
+    def __init__(self, specific_appearance):
+        self.weapon = ''
         self.specific_appearance = specific_appearance
 
     def jump(self):
         print('I jump')
+
+    def assigne_weapon(self, weapon):
+        self.weapon = weapon
 
 
 class Terrorist(Player):
