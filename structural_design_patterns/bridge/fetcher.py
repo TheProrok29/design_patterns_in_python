df3ef8888f3255032538e27a44eb406bf8a7664e
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-24 15:00:56 +0100

    Create abstract Fetcher class.

---
+++ structural_design_patterns/bridge/fetcher.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class Fetcher(ABC):
+
+    def __init__(self, article):
+        self.article = article
+
+    @abstractmethod
+    def get_snippet(self):
+        pass
+
+    @abstractmethod
+    def get_article(self):
+        pass
+
+    @abstractmethod
+    def get_ads(self):
+        pass
+
+    @abstractmethod
+    def get_ads(self):
+        pass
+
+    @abstractmethod
+    def go_to_full_version(self):
+        pass
