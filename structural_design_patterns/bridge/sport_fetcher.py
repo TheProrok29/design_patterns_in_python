a3e67b62edbb86c9bf6d9cb24af4589a5bdd7efa
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-24 15:06:53 +0100

    Add SportFetcher inherit from Fetcher.

---
+++ structural_design_patterns/bridge/sport_fetcher.py
@@ -0,0 +1,15 @@
+from fetcher import Fetcher
+
+
+class SportFetcher(Fetcher):
+    def get_snippet(self):
+        return self.article[:100]
+
+    def get_article(self):
+        return self.article
+
+    def get_ads(self):
+        return 'Some sports ads'
+
+    def go_to_full_version(self):
+        return 'Buy full version now!'
