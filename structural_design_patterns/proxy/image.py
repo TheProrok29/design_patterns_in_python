714bfc043ffe0d84785257f1e89fdabdd244f332
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-25 15:55:55 +0100

    Add ProxyImage class.

--- structural_design_patterns/proxy/image.py
+++ structural_design_patterns/proxy/image.py
@@ -20,3 +20,18 @@ class Image(AbstractImage):
 
     def display_image(self):
         print('Display image.')
+
+
+class ProxyImage(AbstractImage):
+    def __init__(self, image):
+        super().__init__(image.file_path)
+        self.image_loaded = False
+        self.image = image
+
+    def load_image(self):
+        self.image.load_image()
+
+    def display_image(self):
+        if not self.image_loaded:
+            self.load_image()
+        self.image.display_image()  
