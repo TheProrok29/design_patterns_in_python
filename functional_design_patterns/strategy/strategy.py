3dac69d1e6aaef63104f94cddb583f816670b94e
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-26 17:55:50 +0100

    Add strategy file with sum_list function.

---
+++ functional_design_patterns/strategy/strategy.py
@@ -0,0 +1,10 @@
+from sums import *
+
+
+def sum_list(int_list: List[int]):
+    if len(int_list) < 100:
+        return sum_list1(int_list)
+    elif len(int_list) < 10 ** 5:
+        return sum_list3(int_list)
+    else:
+        return sum_list2(int_list)
