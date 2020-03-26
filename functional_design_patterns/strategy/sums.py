a3da934341206dbf51dcd59707ed733a99c2c3b5
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-26 17:46:00 +0100

    Add print with function was chose.

--- functional_design_patterns/strategy/sums.py
+++ functional_design_patterns/strategy/sums.py
@@ -6,12 +6,15 @@ def sum_list1(int_list: List[int]) -> int:
     result_sum = 0
     for num in int_list:
         result_sum += num
+    print('sum_list1')
     return result_sum
 
 
 def sum_list2(int_list: List[int]) -> int:
+    print('sum_list2')
     return sum(int_list)
 
 
 def sum_list3(int_list: List[int]) -> int:
+    print('sum_list3')
     return functools.reduce(lambda x, y: x + y, int_list)
