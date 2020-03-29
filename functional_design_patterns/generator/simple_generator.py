fba089c0167c297a10ab0d3cc7737b0973e19980
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-29 22:53:58 +0200

    Add simple_generator file.

---
+++ functional_design_patterns/generator/simple_generator.py
@@ -0,0 +1,15 @@
+def generator():
+    yield 1
+    # print('a')
+    yield 2
+    # print('b')
+    yield 3
+    # print('c')
+
+my_generator = generator()
+
+for element in my_generator:
+    print(element, end=' ')
+
+for element in my_generator:
+    print(element, end=' ')
