bb7d613ea9b10561d06c7103200356c814270118
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-30 10:25:43 +0200

    Add main example file.

---
+++ functional_design_patterns/template_method/main.py
@@ -0,0 +1,9 @@
+from accountant import Accountant
+from secretary import Secretary
+
+secretary = Secretary('Marie')
+accountant = Accountant('Elen')
+
+secretary.work_day()
+print('////////')
+accountant.work_day()
