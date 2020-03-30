806c5c44640cc071683740ac497f5133f9018f7a
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-30 15:26:50 +0200

    Add Director class inherit from TeamMember.

---
+++ functional_design_patterns/chain_of_responsibility/director.py
@@ -0,0 +1,21 @@
+from team_member import TeamMember
+
+
+class Director(TeamMember):
+    def process(self, task):
+        if 'meet' in task:
+            if 'president' in task:
+                self.meet_the_president()
+                return True
+            elif 'minister' in task:
+                self.meet_the_prime_minister()
+                return True
+        return False
+
+    @staticmethod
+    def meet_the_president():
+        print('Meet the president')
+
+    @staticmethod
+    def meet_the_prime_minister():
+        print('Meet the Prime Minister')
