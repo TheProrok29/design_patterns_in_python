a338c712371637470af41dc2bf7a34a7021ae2db
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-30 16:46:29 +0200

    Add Dialog widget class.

---
+++ functional_design_patterns/chain_of_responsibility/event_widget_example/dialog.py
@@ -0,0 +1,15 @@
+from widget import Widget
+
+
+class Dialog(Widget):
+    @staticmethod
+    def handle_ok(event):
+        print(f'Accepted by event: {event}')
+
+    @staticmethod
+    def handle_cancel(event):
+        print(f'Canceled by event: {event}')
+
+    @staticmethod
+    def handle_send(event):
+        print(f'Send by event: {event}')
