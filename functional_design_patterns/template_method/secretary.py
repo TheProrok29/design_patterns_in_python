9ae2e2ddefa26e00d9d53fda47da1a5a2c539948
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-30 10:25:20 +0200

    Add Secretary class inherit from Worker.

---
+++ functional_design_patterns/template_method/secretary.py
@@ -0,0 +1,16 @@
+from worker import Worker
+
+
+class Secretary(Worker):
+
+    def prepare_for_work(self):
+        print('Prepare secretary for work')
+
+    def go_to_work(self):
+        print('Go to secretary work')
+
+    def start_up(self):
+        print('Start work as secretary')
+
+    def do_work(self):
+        print('Call someone')
