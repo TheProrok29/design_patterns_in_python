d0a6d9228a9f3eea0b3d58f5080deee1345044f7
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-31 21:48:14 +0200

    Add name property and __str__() to Worker class.

--- functional_design_patterns/observer/worker.py
+++ functional_design_patterns/observer/worker.py
@@ -4,12 +4,16 @@ from observable import Observable
 
 
 class Worker(Observable):
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
         self.progress = 0
         self.limit = 100
+        self._name = name
 
     def do_work(self, *args, **kwargs):
         self.progress += randint(1, 10)
         self.progress = self.limit if self.progress > self.limit else self.progress
-        self.notify(self, *args, **kwargs)
+        self.notify(self.progress)
+
+    def __str__(self):
+        return self._name
