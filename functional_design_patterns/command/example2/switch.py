222261c3d6f436826fcc724ced4b1c01d0cdcdc8
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-31 20:44:50 +0200

    Add Switch class for second example.

---
+++ functional_design_patterns/command/example2/switch.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+
+class Switch:
+    def __init__(self):
+        self.history = deque()
+
+    def execute(self, command):
+        self.history.appendleft(command)
+        command.execute()
+
+    def get_history(self):
+        return self.history
