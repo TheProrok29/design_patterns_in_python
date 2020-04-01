1543b78c03208b58f3937a5c75adf7349825a913
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-01 09:49:40 +0200

    Add abstract class Animal and Dog and Cat class inherit from it.

---
+++ other_design_patterns/visitor/animals.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+
+class Animal(ABC):
+    '''Complicated class'''
+    pass
+
+
+class Dog(Animal):
+    '''Complicated class'''
+    pass
+
+
+class Cat(Animal):
+    '''Complicated class'''
+    pass
