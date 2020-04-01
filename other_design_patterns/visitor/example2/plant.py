e41a4f6442e02a8b6cb260a56e65a0c1afe826c0
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-01 11:47:29 +0200

    Add abstract class Plant and 3 class inherit from Plant: Grass, Cherry and Nettle.

---
+++ other_design_patterns/visitor/example2/plant.py
@@ -0,0 +1,30 @@
+from abc import ABC
+
+
+class Plant(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    def accept_visitor(self, visitor):
+        visitor.accept(self)
+
+    def water(self, responsible):
+        print(f'{self.name} watered by {responsible}')
+
+    def ear(self, responsible):
+        print(f'{self.name} eaten by {responsible}')
+
+
+class Grass(Plant):
+    def __init__(self, name):
+        super().__init__('grass')
+
+
+class Cherry(Plant):
+    def __init__(self, name):
+        super().__init__('cherry')
+
+
+class Nettle(Plant):
+    def __init__(self, name):
+        super().__init__('nettle')
