aa8bf57b24c66c29be22eb2037edd39d50b6f012
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-02 13:57:34 +0200

    Add waiter_controller file with WaiterController class as Controller.

---
+++ architectural_design_patterns/mvc/waiter_controller.py
@@ -0,0 +1,19 @@
+from client_view import ClientView
+from menu_model import MenuModel
+
+
+class WaiterController:
+    def __init__(self):
+        self.menu = MenuModel()
+        self.client = ClientView()
+
+    def run(self):
+        self.client.show_menu(self.menu.get_menu())
+        valid_request = False
+        name = ''
+        while not valid_request:
+            name = self.client.request_meal()
+            if len(name) > 4:
+                valid_request = True
+        meal = self.menu.get_menu(name)
+        self.client.show(meal)
