48c8da6e44adfa12c4548e1fe3b73b24cb995134
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-02 14:18:15 +0200

    Add main example file.

--- architectural_design_patterns/mvc/client_view.py
+++ architectural_design_patterns/mvc/client_view.py
@@ -1,6 +1,13 @@
+import time
+
+
 class ClientView:
     def show(self, meal):
-        print(f'Here is my {meal} meal.')
+        if meal is not None:
+            print(f'Here is my {meal} meal.')
+        else:
+            print(f'There is no that meal')
+        time.sleep(5)
 
     def request_meal(self):
         return input('Put here the name of a meal: ')
