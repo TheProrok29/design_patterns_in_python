32fe71109f563e1889d907e710dafe699686910c
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-03 10:49:57 +0200

    Create main example file.

---
+++ other_design_patterns/dependency_Injection/main.py
@@ -0,0 +1,10 @@
+from computer import *
+from processor import *
+
+amd_computer = Computer(AMDProcessor)
+intel_computer = Computer(IntelProcessor)
+arm_computer = Computer(ARMProcessor)
+
+print(amd_computer)
+print(intel_computer)
+print(amd_computer)
