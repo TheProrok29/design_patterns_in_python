643fc070a1806ef10d97aceb0a9a55529fbf1a12
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-03 10:51:01 +0200

    Add object not class as Computer instance parameter.

--- other_design_patterns/dependency_Injection/main.py
+++ other_design_patterns/dependency_Injection/main.py
@@ -1,9 +1,9 @@
 from computer import *
 from processor import *
 
-amd_computer = Computer(AMDProcessor)
-intel_computer = Computer(IntelProcessor)
-arm_computer = Computer(ARMProcessor)
+amd_computer = Computer(AMDProcessor())
+intel_computer = Computer(IntelProcessor())
+arm_computer = Computer(ARMProcessor())
 
 print(amd_computer)
 print(intel_computer)
